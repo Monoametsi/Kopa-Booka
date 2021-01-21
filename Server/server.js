e2aafaeb79700fd4c6a9075256fdce664fd752e6
Junior <216026633@student.uj.ac.za>
2021-01-21 20:31:12 +0200

    Completing initialisation

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,6 @@
-console.log('We live!!!!!');
+const express = require('express');
+const app = express();
+
+const dirname = __dirname.slice(0, __dirname.search(/Server/i) - 1);
+
+console.log(dirname);
