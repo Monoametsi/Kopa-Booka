c5f7dae96ec6d97f6477bfbc2409a6abf2daa914
Junior <216026633@student.uj.ac.za>
2021-01-22 00:50:51 +0200

    Added docs to server

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const path = require('path');
+const url = require('url');
 const app = express();
+const dirname = __dirname.slice(0, __dirname.search(/\\Server/i));
 
-const dirname = __dirname.slice(0, __dirname.search(/Server/i) - 1);
+app.use(express.static(path.join(dirname)));
 
-console.log(dirname);
+const homePath = path.join(dirname, 'Home', 'Home-HTML', 'index.html');
+
+app.get('/', (req, res) => {
+	res.sendFile(homePath);
+});
+
+const PORT = process.env.PORT || 8500;
+
+app.listen(PORT, () => {
+	console.log('WE LIVE!!!!!')
+});
