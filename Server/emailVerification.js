276d56a6b4a0e71f0d32936512cc08ea32035407
Junior <216026633@student.uj.ac.za>
2021-04-20 10:29:15 +0200

    Changes and additions

--- Server/emailVerification.js
+++ Server/emailVerification.js
@@ -1,26 +1,40 @@
 const nodeMailer = require('nodemailer');
+const { google } = require('googleapis');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const path = require('path');
-
+//https://t.co/dRW7cCihOu?amp=1
 dotenv.config({path:path.join(__dirname, '.env')});
+
+const CLIENT_ID = process.env.CLIENT_ID.toString();
+const CLIENT_SECRET = process.env.CLIENT_SECRET.toString();
+const REDIRECT_URL = process.env.REDIRECT_URL.toString();
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN.toString();
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+let accessToken = async () => {
+	const accessToken = await oAuth2Client.getAccessToken();
+	return accessToken;
+}
 
 let transporter = nodeMailer.createTransport({
 	service: 'gmail',
-	host: 'smtp.gmail.com',
-	secureConnection: true,
 	auth: {
+		type: 'OAuth2',
 		user: 'pt.projects.submission@gmail.com',
-		pass: process.env.EmailPassword.toString()
-	},
-	tls: {
-        ciphers: 'SSLv3'
-    }
+		clientId: CLIENT_ID,
+		clientSecret: CLIENT_SECRET,
+		refreshToken: REFRESH_TOKEN,
+		accessToken: accessToken()
+	}
 });
 
 let success = true;
 
-const mailDeliverer = (email, ress) => {
+const mailDeliverer = async (email, ress) => {
+
 	let token = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, {
 		expiresIn: '3d'
 	});
