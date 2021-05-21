5b45f88197028ba805918fa3b59b20dce7688649
Junior <216026633@student.uj.ac.za>
2021-05-21 16:37:45 +0200

    Changes and additions

--- Server/place-advert.js
+++ Server/place-advert.js
@@ -43,7 +43,7 @@ let placeAdvert = async (req, res) => {
 
 		if(files.uploaded.length === undefined){
 			let oldPath = files.uploaded.path;
-			let newPath = dirname + '\\imageUploads\\' + files.uploaded.name;
+			let newPath = dirname + '/imageUploads/' + files.uploaded.name;
 			fs.readFile(oldPath, function (err, data) {
 					if (err) throw err;
 					console.log('File read!');
@@ -64,7 +64,7 @@ let placeAdvert = async (req, res) => {
 		}else if(files.uploaded.length >= 1){
 			for(var i = 0; i < files.uploaded.length; i++){
 				let oldPath = files.uploaded[i].path;
-				let newPath = dirname + '\\imageUploads\\' + files.uploaded[i].name;
+				let newPath = dirname + '/imageUploads/' + files.uploaded[i].name;
 				 fs.readFile(oldPath, function (err, data) {
 					if (err) throw err;
 					console.log('File read!');
